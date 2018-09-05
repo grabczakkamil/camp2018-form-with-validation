7c1e6291ad19f5bd149ac78f3033bed680205f5b
Kamil Grabczak <grabczakkamil@gmail.com>
2018-09-05 23:43:41 +0200

    change get all form elements by class name to tag name

--- src/js/app.js
+++ src/js/app.js
@@ -4,11 +4,10 @@
 
     formEfi.addEventListener("submit", (event) => {
         event.preventDefault();
-        console.log('');
 
         const invalidInputStyle = document.getElementsByClassName('is-invalid');
         const paragraphsWithFeedback = document.getElementsByClassName('invalid-feedback');
-        const validData = document.getElementsByClassName("validation");
+        const validData = document.forms[0].getElementsByTagName('input');
         const validDataLength = validData.length;
 
 
@@ -59,14 +58,14 @@
 
             if (validData[i].classList.contains('validation-required')) {
 
-                if (validData[i].type != "checkbox" && validData[i].value === "") {
+                if (validData[i].type !== "checkbox" && validData[i].value === "") {
 
                     addClassName("is-invalid");
                     inputParent.appendChild(feedbackMessage(textDependsOnInputType()));
 
-                } else if (validData[i].type == "checkbox") {
+                } else if (validData[i].type === "checkbox") {
 
-                    if (validData[i].checked == false) {
+                    if (validData[i].checked === false) {
                         addClassName("is-invalid");
                         inputParent.appendChild(feedbackMessage(textDependsOnInputType()));
 
@@ -74,22 +73,23 @@
                         addClassName("is-valid");
                     }
 
-                } else if (validData[i].name == "username") {
+                } else if (validData[i].name === "username") {
                     validationWithRegularExpresion("^[^ A-Z]+$", "Nazwa użytkownika może składać się z małych liter i nie może zawierać spacji");
 
-                } else if (validData[i].name == "amount") {
+                } else if (validData[i].name === "amount") {
                     validationWithRegularExpresion("^[1-9]{1}$|^[1-9]{1}[0-9]{1}$|^100$", "Kwota to liczba z przedziału 1-100");
                 }
 
             } else if (validData[i].classList.contains('validation-not-required') && validData[i].value !== '') {
 
-                if (validData[i].name == "email") {
+                if (validData[i].name === "email") {
                     validationWithRegularExpresion("^[a-zA-Z\\d][\-\\w\.]+@([a-zA-Z\\d]+[\-a-zA-Z\\d]+\.)+[a-zA-Z]+$", "Nieprawidłowy adres e-mail");
 
-                } else if (validData[i].name == "pin") {
+                } else if (validData[i].name === "pin") {
                     validationWithRegularExpresion("^[0-9]{1,8}$", "Pin musi składać się maksymalnie z 8 cyfr");
                 }
             }
         }
     });
+    console.log(document.getElementsByClassName('validation'));
 })();
