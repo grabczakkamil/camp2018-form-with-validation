685be286d8cd3675491bbc2cdee31156771bcb19
Kamil Grabczak <grabczakkamil@gmail.com>
2018-09-13 23:48:42 +0200

    add event change (all inputs)

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,10 @@
     'use strict';
 
     const validationConfig = {
-        noCheckbox: {
-            "alert": "Uzupełnij to pole"
-        },
+
         username: {
-            "RegExp": "^[^ A-Z]+$",
+            "fieldSelector": "[name='username']",
+            "RegExp": "^[a-z]+$",
             "alert": "Nazwa użytkownika może składać się z małych liter i nie może zawierać spacji"
         },
         email: {
@@ -24,21 +23,20 @@
         },
         checkbox: {
             "alert": "Musisz zaznaczyć tę zgodę"
+        },
+        noCheckbox: {
+            "alert": "Uzupełnij to pole"
         }
     };
-    
-          function addClassName(className) {
-            inputName.classList.add(className);
-        };
 
-    function validationWithRegularExpresion(regularExpresionValue) {
+    function validationWithRegularExpresion(validationInputValue, regularExpresionValue) {
         let patternFormField = new RegExp(regularExpresionValue);
 
-        if (patternFormField.test(inputName.value) === false) {
-            addClassName("is-invalid");
+        if (patternFormField.test(validationInputValue.value) === false) {
+            validationInputValue.classList.add("is-invalid");
 
         } else {
-            addClassName("is-valid");
+            validationInputValue.classList.add("is-valid");
         };
     };
 
@@ -53,36 +51,54 @@
     const formEfi = document.getElementById("form-efi");
     const invalidInputStyle = document.getElementsByClassName('is-invalid');
     const paragraphsWithFeedback = document.getElementsByClassName('invalid-feedback');
-    const validData = document.forms[0].getElementsByTagName('input');
+    const validData = formEfi.querySelectorAll('input');
     const validDataLength = validData.length;
 
 
-
-    const inputName = document.querySelector('[name="username"]');
-    const inputParent = inputName.parentNode;
-
-    inputName.addEventListener("change", (event) => {
-       validationWithRegularExpresion(validationConfig.username.RegExp);
-        inputParent.appendChild(feedbackMessage(validationConfig.username.alert));
+    for (let i = 0; i < validDataLength; i++) {
 
 
-    });
+        validData[i].addEventListener('change', function () {
+            while (paragraphsWithFeedback[0]) {
+                paragraphsWithFeedback[0].remove();
+            };
 
+            while (invalidInputStyle[0]) {
+                invalidInputStyle[0].classList.remove('is-invalid');
+            };
 
+            if (validData[i].dataset.state === "required") {
 
-    //    validData.forEach(myFunction);
-    //    
-    //    function myFunction() {
-    //    inputName.addEventListener("change", (event) => {
-    //
-    //    
-    //    console.log(inputName);
-    //        
-    //
-    //        });
-    //        
-    //    };
+                if (validData[i].name === "username") {
 
+                    validationWithRegularExpresion(validData[i], validationConfig.username.RegExp);
+                    validData[i].parentNode.appendChild(feedbackMessage(validationConfig.username.alert));
+                } else if (validData[i].name === "amount") {
+
+                    if (validData[i].value >= validationConfig.amount.minValue && validData[i].value <= validationConfig.amount.maxValue === true) {
+                        validData[i].classList.add("is-valid");
+                    } else {
+                        validData[i].classList.add("is-invalid");
+                    };
+                    validData[i].parentNode.appendChild(feedbackMessage(validationConfig.amount.alert));
+                }
+
+            } else if (validData[i].dataset.state !== "required" && validData[i].value !== '') {
+
+                if (validData[i].name === "email") {
+
+                    validationWithRegularExpresion(validData[i], validationConfig.email.RegExp);
+                    validData[i].parentNode.appendChild(feedbackMessage(validationConfig.email.alert));
+
+                } else if (validData[i].name === "pin") {
+                    validationWithRegularExpresion(validData[i], validationConfig.pin.RegExp);
+                    validData[i].parentNode.appendChild(feedbackMessage(validationConfig.pin.alert));
+                }
+            } else {
+                console.log("COŚ INNEGO");
+            }
+        });
+    };
 
 
 
